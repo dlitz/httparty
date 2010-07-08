48224f0615b32133afcff4718ad426df7a4b401b
Dwayne C. Litzenberger <dlitz@dlitz.net>
2010-07-08 19:25:43 -0400

    Fix "superclass mismatch for class CustomParser (TypeError)" when running tests
    This error would occur when spec/httparty_spec.rb was loaded before
    spec/httparty/parser_spec.rb.
    
    This is less likely to happen on OSX than on Linux, because Dir.glob on OSX
    returns items sorted alphabetically, whereas on Linux the ordering depends on
    the underlying filesystem ordering.

--- spec/httparty/parser_spec.rb
+++ spec/httparty/parser_spec.rb
@@ -1,7 +1,5 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 
-class CustomParser < HTTParty::Parser; end
-
 describe HTTParty::Parser do
   describe ".SupportedFormats" do
     it "returns a hash" do
